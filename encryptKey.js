d15e2ab025346d11f31b0550e2ec6df6f270ef7e
theevillane <brianochiengonyango2@gmail.com>
2025-03-25 00:11:27 +0300

    new updates

--- encryptKey.js
+++ encryptKey.js
@@ -1,4 +1,4 @@
-const ethers = require("ethers");
+const { ethers } = require("ethers");
 const fs = require("fs-extra");
 require("dotenv").config();
 
@@ -6,8 +6,8 @@ async function main() {
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
 
   const encryptedJsonKey = await wallet.encrypt(
-    process.env.PRIVATE_KEY_PASSWORD,
-    process.env.PRIVATE_KEY
+    process.env.PRIVATE_KEY_PASSWORD
+    //process.env.PRIVATE_KEY
   );
   // In later version (^6.2.3 as of this commit) of etherjs, PRIVATE_KEY is inferred from wallet, so there is no need to
   // pass private key again.
